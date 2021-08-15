6fdc55d722c6f9aa104c0c30cabb9609c85c207e
ShuyunLiuRun <lsy2601162193@gmail.com>
2021-08-15 23:42:53 +1200

    Validations and unit tests

--- src/services/convertXml.tsx
+++ src/services/convertXml.tsx
@@ -25,6 +25,7 @@ const getFileName = (eachCSV: string[]) => {
 	return eachCSV[1].split(",")[1];
 };
 
+// push each row of the xml into an array
 const formToArray = (dataFromForm: string) => {
 	var xmlParser = new DOMParser();
 	var xmlDoc = xmlParser.parseFromString(dataFromForm, "text/xml");
@@ -51,8 +52,11 @@ const getTailer = (xmlDocArr: string[]) => {
 	return tailer;
 };
 
-const toCSVArraies = (unformattedRows: string[]) => {
+export const toCSVArraies = (unformattedRows: string[]) => {
+	console.log(unformattedRows);
+
 	let CSVArraies: string[][] = [];
+	//seperate the chunks by the "200"
 	let seperatedData = seperateRowsToChunks(unformattedRows);
 	seperatedData.forEach((chunk) => {
 		let eachCSV: string[] = [sharedHeaders];
@@ -73,14 +77,14 @@ const seperateRowsToChunks = (unformattedRows: string[]) => {
 	let chunks: string[][] = [];
 	let quickPointer = 0;
 	let slowPointer = 0;
+
 	let validatedRows = validateRows(unformattedRows);
 	validatedRows.forEach((row, i) => {
 		if (row.startsWith("200") || row.includes("900")) {
 			if (quickPointer === 0) {
 				quickPointer = i + 1;
 				slowPointer = i + 1;
-				// to make sure it contains at least 1 row for each 200
-			} else if (i - slowPointer > 1) {
+			} else {
 				quickPointer = i + 1;
 				chunks.push(unformattedRows.slice(slowPointer, quickPointer));
 				slowPointer = i + 1;
@@ -90,8 +94,64 @@ const seperateRowsToChunks = (unformattedRows: string[]) => {
 	return chunks;
 };
 
-//to check whether the row is validate
+//to check whether the rows are validate
 const validateRows = (unformattedRows: string[]) => {
+	const validateOneRowForEach = (unformattedRows: string[]) => {
+		let one = false;
+		let two = false;
+		let three = false;
+		let nine = false;
+		unformattedRows.forEach((row) => {
+			if (row.startsWith("100")) one = true;
+			if (row.startsWith("200")) two = true;
+			if (row.startsWith("300")) three = true;
+			if (row.startsWith("900")) nine = true;
+		});
+		return one && two && three && nine;
+	};
+
+	const validateOnlyAppearOnce = (unformattedRows: string[]) => {
+		let one = 0;
+		let nine = 0;
+		unformattedRows.forEach((row) => {
+			if (row.startsWith("100")) one += 1;
+			if (row.startsWith("900")) nine += 1;
+		});
+		return one === 1 && nine === 1;
+	};
+
+	const validateWithinHeadAndTail = (unformattedRows: string[]) => {
+		let two = false;
+		let three = false;
+		unformattedRows.forEach((row, i) => {
+			if (row.startsWith("200"))
+				two = i > 0 && i < unformattedRows.length - 1;
+			if (row.startsWith("300"))
+				three = i > 0 && i < unformattedRows.length - 1;
+		});
+		return two && three;
+	};
+
+	try {
+		if (validateOneRowForEach(unformattedRows) === false) {
+			throw new Error(
+				'The CSVIntervalData element should contain at least 1 row for each of "100", "200", "300","900"'
+			);
+		}
+		if (validateOnlyAppearOnce(unformattedRows) === false) {
+			throw new Error(
+				"'100', '900' rows should only appear once inside the CSVIntervalData element"
+			);
+		}
+		if (validateWithinHeadAndTail(unformattedRows) === false) {
+			throw new Error(
+				'"200" and "300" can repeat and should be within the header and trailer rows'
+			);
+		}
+	} catch (e) {
+		console.error(e);
+	}
+
 	let validatedRows = unformattedRows.filter(
 		(row) =>
 			row.startsWith("100") ||
---
+++ src/test/convertXml.test.js
@@ -0,0 +1,24 @@
+import { toCSVArraies } from "../services/convertXml";
+
+// to test if the CSVIntervalData element contain at least 1 row for each
+test("case one", () => {
+	expect(
+		toCSVArraies(["200,12345678901,E1,E1,E1,N1,HGLMET501,KWH,30,"])
+	).toThrow();
+});
+
+// to test if header or tailer appeared more than once
+test("case two", () => {
+	expect(toCSVArraies(["900", "900"])).toThrow();
+});
+
+// to test '200' or '300' be within the header and trailer rows
+test("case three", () => {
+	expect(
+		toCSVArraies([
+			"200,12345678901,E1,E1,E1,N1,HGLMET501,KWH,30,",
+			"100,NEM12,201801211010,MYENRGY,URENRGY",
+			"900"
+		])
+	).toThrow();
+});
